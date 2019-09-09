87a575f4f0112ff4b21f5bc856dc0a9a27acd210
hu <hu@qq.com>
2019-09-09 17:10:11 +0800

    first commit

---
+++ SayHello.java
@@ -0,0 +1,16 @@
+public class SayHello {
+	public static void main (String[] args) {
+		System.out.println( " Below is the function of sayhello");
+		sayHello(0);
+		
+		
+		
+	public static void sayHello(int sum){
+		System.out.println("Say Hello!"+sum);
+	}
+	
+	
+	public static int sum(){
+		return  1+3;
+	}
+}
